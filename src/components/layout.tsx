74323c88ad90d0c8de3c79cf830543c5d908c82e
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2022-01-23 12:40:10 +0200

    create auth provider and token handlers

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,15 @@
 import { FunctionComponent, ReactNode } from "react";
 import Link from "next/link";
-// import { useAuth } from "src/auth/useAuth";
+import { useAuth } from "src/auth/useAuth";
 
 interface IProps {
   main: ReactNode;
 }
 
 const Layout: FunctionComponent<IProps> = ({ main }) => {
-  const authenticated = false;
-  const logout = () => {};
+  const { user, logout, authenticated } = useAuth();
   const navHeight = "64px";
+
   return (
     <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
       <nav className="bg-gray-800 " style={{ height: navHeight }}>
