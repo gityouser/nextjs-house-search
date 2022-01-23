c5ea3b8ee7a3ab8cd4c1bc8cfdd2392e57e5ad65
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2022-01-23 14:15:45 +0200

    implement auth page

--- src/auth/firebaseAdmin.ts
+++ src/auth/firebaseAdmin.ts
@@ -1,4 +1,34 @@
-// import * as admin from "firebase-admin";
-// import { NextApiRequest } from "next";
+import * as admin from "firebase-admin";
+import { NextApiRequest } from "next";
 
-export {};
+const verifyIdToken = (token: string) => {
+  const firebasePrivateKey: string = process.env.FIREBASE_PRIVATE_KEY ?? "";
+
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: firebasePrivateKey.replace(/\\n/g, "\n"),
+      }),
+    });
+  }
+
+  return admin
+    .auth()
+    .verifyIdToken(token)
+    .catch((e) => {
+      console.log("%cError verifying token", "color: red", e);
+      return null;
+    });
+};
+export const loadIdToken = async (
+  req: NextApiRequest
+): Promise<string | null> => {
+  if (!req.cookies.token) return null;
+  const decodedToken = await verifyIdToken(req.cookies.token);
+  console.log("%c Decoded token", "color: green", decodedToken);
+  if (!decodedToken) return null;
+
+  return decodedToken.uid;
+};
--- src/components/firebaseAuth.tsx
+++ src/components/firebaseAuth.tsx
@@ -1,6 +1,38 @@
-// import { FunctionComponent, useState, useEffect } from "react";
-// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-// import firebase from "firebase/app";
-// import "firebase/auth";
+import { FunctionComponent, useState, useEffect } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase/app";
+import "firebase/auth";
 
-export {};
+const firebaseAuthConfig = {
+  signInFlow: "popup",
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: false,
+    },
+  ],
+  signInSuccessUrl: "/",
+};
+
+const FirebaseAuth: FunctionComponent = () => {
+  const [renderAuth, setRenderAuth] = useState(false);
+
+  useEffect(() => {
+    // make sure firebase is only run in the browser
+    // and this is not server rendered
+    setRenderAuth(true);
+  }, []);
+
+  return (
+    <div className="mt-16">
+      {renderAuth ? (
+        <StyledFirebaseAuth
+          uiConfig={firebaseAuthConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default FirebaseAuth;
