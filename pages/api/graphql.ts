604572c97c73dfad8d1bca08a13f4501c812f75b
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2022-01-28 22:27:58 +0200

    setup the graphql server

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,9 +1,30 @@
-// import "reflect-metadata";
-// import { NextApiRequest } from "next";
-// import { ApolloServer } from "apollo-server-micro";
-// import { schema } from "src/schema";
-// import { Context } from "src/schema/context";
-// import { prisma } from "src/prisma";
-// import { loadIdToken } from "src/auth/firebaseAdmin";
+import "reflect-metadata";
+import { NextApiRequest } from "next";
+import { ApolloServer } from "apollo-server-micro";
+import { schema } from "src/schema";
+import { Context } from "src/schema/context";
+import { prisma } from "src/prisma";
+import { loadIdToken } from "src/auth/firebaseAdmin";
 
-export {};
+const server = new ApolloServer({
+  schema,
+  context: async ({ req }: { req: NextApiRequest }): Promise<Context> => {
+    const uid = await loadIdToken(req);
+
+    return {
+      uid,
+      prisma,
+    };
+  }, // chunk of data passed to every query
+});
+
+// the handler tels the server to listen to a certain endpoint
+const handler = server.createHandler({ path: "/api/graphql" });
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default handler;
