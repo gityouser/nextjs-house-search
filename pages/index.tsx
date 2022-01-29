8e92560b688ca2ebaead59cd16489cdfac3b3410
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2022-01-29 13:03:35 +0200

    remove test query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-import { useQuery, gql } from "@apollo/client";
+// import { useQuery, gql } from "@apollo/client";
 // import { useDebounce } from "use-debounce";
 import Layout from "src/components/layout";
 import Map from "src/components/map";
@@ -8,15 +8,7 @@ import Map from "src/components/map";
 // import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery, HousesQueryVariables } from "src/generated/HousesQuery";
 
-const HELLO_QUERY = gql`
-  query HelloQuery {
-    hello
-  }
-`;
-
 export default function Home() {
-  const { data, loading } = useQuery(HELLO_QUERY);
-
   const navHeight = "64px";
   return (
     <Layout
@@ -30,7 +22,6 @@ export default function Home() {
             }}
           >
             HouseList
-            <pre>{JSON.stringify(data, null, 2)}</pre>
           </div>
           <div className="w-1/2">{<Map />}</div>
         </div>
