7a5bc9f8d91035b1112d3091ee53ad3922d4dbce
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2022-01-29 13:02:57 +0200

    setup apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,23 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-// import { ApolloProvider } from "@apollo/client";
-// import { useApollo } from "src/apollo";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "src/apollo";
 import { AuthProvider } from "src/auth/useAuth";
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const client = useApollo();
+
   return (
     <AuthProvider>
-      <Head>
-        <title>Home Sweet Home</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <ApolloProvider client={client}>
+        <Head>
+          <title>Home Sweet Home</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <Component {...pageProps} />
+        <Component {...pageProps} />
+      </ApolloProvider>
     </AuthProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-// import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
 // import { useDebounce } from "use-debounce";
 import Layout from "src/components/layout";
 import Map from "src/components/map";
@@ -8,7 +8,15 @@ import Map from "src/components/map";
 // import { useLocalState } from "src/utils/useLocalState";
 // import { HousesQuery, HousesQueryVariables } from "src/generated/HousesQuery";
 
+const HELLO_QUERY = gql`
+  query HelloQuery {
+    hello
+  }
+`;
+
 export default function Home() {
+  const { data, loading } = useQuery(HELLO_QUERY);
+
   const navHeight = "64px";
   return (
     <Layout
@@ -22,6 +30,7 @@ export default function Home() {
             }}
           >
             HouseList
+            <pre>{JSON.stringify(data, null, 2)}</pre>
           </div>
           <div className="w-1/2">{<Map />}</div>
         </div>
